c194a38d9f1581dde7003170ba4a241e8bd47149
Daniil Votintsev <daniil43russ@gmail.com>
2026-04-23 16:31:42 +0300

    remove

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,1 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- require("nvim-tree").setup({
---   filters = {
---     dotfiles = true,
---     git = {
---       enable = true,
---       ignore = false,
---       timeout = 500,
---     },
---   },
--- })
