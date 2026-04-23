7d9b4fa73f0894e35200c4de1d334335831305fb
Daniil Votintsev <daniil43russ@gmail.com>
2026-04-23 16:30:01 +0300

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -29,3 +29,22 @@ nvm use lts >/dev/null 2>&1
 #     printf '%s@%s %s%s%s > ' $USER $hostname \
 #         (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 # end
+#
+
+set -x GOPATH $HOME/go
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,26 +1,5 @@
 -- In your LSP config file (e.g., ~/.config/nvim/lua/plugins/lsp.lua)
 return {
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = {
-  --     ensure_installed = {
-  --       "bash-language-server",
-  --       "css-lsp",
-  --       "eslint-lsp",
-  --       "gopls",
-  --       "helm-ls",
-  --       "html-lsp",
-  --       "json-lsp",
-  --       "jdtls",
-  --       "marksman",
-  --       "phpactor",
-  --       "typescript-language-server",
-  --       "vue-language-server",
-  --       "tsserver",
-  --       "volar",
-  --     },
-  --   },
-  -- },
   {
     "neovim/nvim-lspconfig",
     keys = {
@@ -63,6 +42,7 @@ return {
           },
         },
         volar = {},
+        gopls = {},
       },
       setup = {
         volar = function()
